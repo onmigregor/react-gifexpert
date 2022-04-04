6fd12114d5758f006df27c9a52c913315b76fc8a
Omar ramirez <oramirez@teravisiontech.com>
2022-04-04 16:22:01 -0400

    first commit

---
+++ src/components/AddCategory.js
@@ -0,0 +1,42 @@
+import React, {useState} from 'react'
+import { PropTypes } from "prop-types";
+
+export const AddCategory = ( { setCategories } ) => {
+
+    const [inputValue, setinputValue] = useState('');
+
+
+    const handleInputChange = (e) =>{
+        setinputValue (e.target.value);
+    }
+
+    const handleSubmit = (e) =>{
+        e.preventDefault()
+        if (inputValue.trim().length >3) {
+            
+            setCategories( categories =>  {
+                return [ inputValue ,...categories ];
+            });
+
+            setinputValue('');
+        }
+    }
+
+    return (
+        <form onSubmit={ handleSubmit }>
+            <input 
+                type="text"
+                value={inputValue}
+                onChange={ handleInputChange }
+            />
+            <button>Agregar Categoria</button>
+        </form>
+  )
+}
+
+
+
+AddCategory.propTypes = {
+    setCategories:PropTypes.func.isRequired
+}
+
